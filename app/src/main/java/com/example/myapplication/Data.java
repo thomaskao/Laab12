8c81ce296bc322691e1233c9258f59b8f551d844
thomaskao <thomaskao99@gmail.com>
2022-01-07 19:33:39 +0800

    first

---
+++ app/src/main/java/com/example/myapplication/Data.java
@@ -0,0 +1,16 @@
+package com.example.myapplication;
+
+import javax.xml.transform.Result;
+
+public class Data {
+    Result result;
+    class Result{
+        public String Station;
+        public String Destination;
+        Result[] results;
+        class Results{
+            String Station;
+            String Destination;
+        }
+    }
+}
